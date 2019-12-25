2ca62cbba5248e89a369cfb7b23a1402166f9978
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-12-25 22:00:30 +0100

    [delivery2mavencentral]  changes for codehouse license plugin
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/main/java/com/github/plantuml/maven/PlantUMLMojo.java
+++ src/main/java/com/github/plantuml/maven/PlantUMLMojo.java
@@ -1,38 +1,40 @@
-/**
- * Copyright 2012 Julien Eluard
- * This project includes software developed by Julien Eluard: https://github.com/jeluard/
- *
+package com.github.plantuml.maven;
+
+/*-
+ * #%L
+ * Maven PlantUML plugin
+ * %%
+ * Copyright (C) 2011 - 2019 Julien Eluard
+ * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * #L%
  */
-package com.github.plantuml.maven;
-
-import java.io.File;
-import java.nio.file.Path;
-import java.util.Iterator;
-import java.util.List;
 
 import net.sourceforge.plantuml.FileFormat;
+import net.sourceforge.plantuml.FileFormatOption;
 import net.sourceforge.plantuml.GeneratedImage;
 import net.sourceforge.plantuml.Option;
 import net.sourceforge.plantuml.OptionFlags;
-import net.sourceforge.plantuml.FileFormatOption;
 import net.sourceforge.plantuml.SourceFileReader;
 import net.sourceforge.plantuml.preproc.Defines;
-
 import org.apache.maven.model.FileSet;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.codehaus.plexus.util.FileUtils;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * @phase generate-resources

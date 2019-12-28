17f7df51dea56e6276aa73f580526eaca8d20ef1
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-12-28 14:59:20 +0100

    [feature/#5-unittests] first running unit test added
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/test/java/com/github/plantuml/maven/PlantUMLMojoTest.java
+++ src/test/java/com/github/plantuml/maven/PlantUMLMojoTest.java
@@ -1,4 +1,4 @@
-//package com.github.plantuml.maven;
+package com.github.plantuml.maven;
 
 /*-
  * #%L
@@ -9,9 +9,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,11 +19,56 @@
  * limitations under the License.
  * #L%
  */
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
 
-//
-//class PlantUMLMojoTest extends AbstractMavenIntegrationTestCase{
-//
-//}
+import org.apache.maven.plugin.testing.MojoRule;
+import org.apache.maven.plugin.testing.WithoutMojo;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class PlantUMLMojoTest {
+
+
+    @Rule
+    public MojoRule rule = new MojoRule() {
+
+        @Override
+        protected void before() throws Throwable {
+        }
+
+        @Override
+        protected void after() {
+        }
+    };
+
+    /**
+     * @throws Exception if any
+     */
+    @Test
+    public void testSomething()
+            throws Exception {
+//        File pom = rule.getTestFile("src/test/resources/unit/project-to-test/pom.xml");
+        File pom = Paths.get("src/test/resources/unit/project-to-test/pom.xml").toFile();
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+        final PlantUMLMojo myMojo = (PlantUMLMojo) rule.lookupMojo("generate", pom);
+        assertNotNull(myMojo);
+        myMojo.execute();
+
+    }
+
+    /**
+     * Do not need the MojoRule.
+     */
+    @WithoutMojo
+    @Test
+    public void testSomethingWhichDoesNotNeedTheMojoAndProbablyShouldBeExtractedIntoANewClassOfItsOwn() {
+
+    }
+
+}

c72580c3b3b2fc8dd69d04aa76ffef84aafb9cbd
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-12-31 11:11:15 +0100

    [feature/#17_truncatePattern] unit test changed to takari test framework
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/test/java/com/github/plantuml/maven/PlantUMLMojoUnitTest.java
+++ src/test/java/com/github/plantuml/maven/PlantUMLMojoUnitTest.java
@@ -1,47 +1,55 @@
-//package com.github.plantuml.maven;
-//
-///*-
-// * #%L
-// * Maven PlantUML plugin
-// * %%
-// * Copyright (C) 2011 - 2019 Julien Eluard
-// * %%
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *      http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// * #L%
-// */
-//
-//import org.apache.maven.plugin.testing.AbstractMojoTestCase;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.File;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//
-//import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-//import static org.junit.jupiter.api.Assumptions.assumingThat;
-//
-//
-//// https://maven.apache.org/plugin-developers/plugin-testing.html
-//// https://cwiki.apache.org/confluence/display/MAVEN/Creating+a+Maven+Integration+Test
-//public class PlantUMLMojoUnitTest extends AbstractMojoTestCase {
-//
-//
+package com.github.plantuml.maven;
+
+/*-
+ * #%L
+ * Maven PlantUML plugin
+ * %%
+ * Copyright (C) 2011 - 2019 Julien Eluard
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import io.takari.maven.testing.TestMavenRuntime;
+import io.takari.maven.testing.TestResources;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.File;
+
+import static io.takari.maven.testing.TestMavenRuntime.newParameter;
+import static io.takari.maven.testing.TestResources.assertFilesPresent;
+
+public class PlantUMLMojoUnitTest {
+    @Rule
+    public final TestResources resources = new TestResources("src/test/resources/unit", "target/test-unit");
+
+    @Rule
+    public final TestMavenRuntime maven = new TestMavenRuntime();
+
+    @Test
+    public void test() throws Exception {
+        File basedir = resources.getBasedir("project-to-test");
+        maven.executeMojo(basedir, "generate", newParameter("name", "value"));
+        assertFilesPresent(basedir, "target/plantuml/AblaufManuelleGenerierung.png");
+        assertFilesPresent(basedir, "target/plantuml/QueueStatechart.png");
+    }
+
+
 //    /**
 //     * @throws Exception if any
 //     */
 //    @Test
-//    @DisplayName("Check default values of mojo parameters")
 //    public void testSomething() {
 //        final File pom = Paths.get("src/test/resources/unit/project-to-test/pom.xml").toFile();
 //        assumingThat(pom == null || !pom.exists(), () -> {
@@ -71,4 +79,4 @@
 //        assertNotNull(mojo.truncatePattern);
 //        assertEquals("src/main/*", mojo.truncatePattern);
 //    }
-//}
+}

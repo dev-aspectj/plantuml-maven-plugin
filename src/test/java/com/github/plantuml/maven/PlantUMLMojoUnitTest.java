004b841b836295c1bdcb7321999e3363c2f6660c
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-12-31 12:42:12 +0100

    [feature/#17_truncatePattern] unit test for check of properties finished successful
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/test/java/com/github/plantuml/maven/PlantUMLMojoUnitTest.java
+++ src/test/java/com/github/plantuml/maven/PlantUMLMojoUnitTest.java
@@ -22,28 +22,47 @@ package com.github.plantuml.maven;
 
 import io.takari.maven.testing.TestMavenRuntime;
 import io.takari.maven.testing.TestResources;
-import org.apache.maven.model.Plugin;
-import org.apache.maven.model.PluginExecution;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.project.MavenProject;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.io.File;
-import java.util.Map;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static io.takari.maven.testing.TestMavenRuntime.newParameter;
 import static io.takari.maven.testing.TestResources.assertFilesPresent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 public class PlantUMLMojoUnitTest {
+
     @Rule
     public final TestResources resources = new TestResources("src/test/resources/unit", "target/test-unit");
 
     @Rule
     public final TestMavenRuntime maven = new TestMavenRuntime();
 
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    protected File basedir = null;
+
+    @Before
+    public void setUpTestCase() throws Exception {
+        basedir = resources.getBasedir("project-to-test");
+        // exceptionRule.expect(RuntimeException.class);
+    }
+
+
     @Test
     public void basicTest() throws Exception {
-        File basedir = resources.getBasedir("project-to-test");
         maven.executeMojo(basedir, "generate", newParameter("name", "value"));
         assertFilesPresent(basedir, "target/plantuml/AblaufManuelleGenerierung.png");
         assertFilesPresent(basedir, "target/plantuml/QueueStatechart.png");
@@ -51,33 +70,33 @@ public class PlantUMLMojoUnitTest {
 
     @Test
     public void checkParameter() throws Exception {
-        File basedir = resources.getBasedir("project-to-test");
         final MavenProject mavenProject = maven.readMavenProject(basedir);
-        mavenProject.getBasedir();
-        final Plugin plugin = mavenProject.getPlugin("com.github.funthomas424242:plantuml-maven-plugin");
-        final Map<String, PluginExecution> executionMap = plugin.getExecutionsAsMap();
-//        executionMap.get("");
+        final MavenSession mavenSession = maven.newMavenSession(mavenProject);
+        final MojoExecution mojoExecution = maven.newMojoExecution("generate");
+        final Mojo mojo = maven.lookupConfiguredMojo(mavenSession, mojoExecution);
+        checkProperties((PlantUMLMojo) mojo);
+    }
 
+    protected void checkProperties(final PlantUMLMojo mojo) {
+        assertNotNull(mojo.outputDirectory);
+
+        /* check default values */
+        // check outputDirectory
+        final Path plantumlTargetDir = Paths.get(basedir.getAbsolutePath().toString(),"target/plantuml");
+        assertEquals(plantumlTargetDir.toAbsolutePath().toFile(), mojo.outputDirectory);
+        // outputInSourceDirectory
+        assertFalse(mojo.outputInSourceDirectory);
+        // verbose
+        assertFalse(mojo.verbose);
+
+        /* check required parameters */
+        assertNotNull(mojo.sourceFiles);
+        assertEquals(basedir.getAbsolutePath().toString(), mojo.sourceFiles.getDirectory().toString());
+
+        /* check others parameters */
+        assertNotNull(mojo.truncatePattern);
+        assertEquals("src/main/*", mojo.truncatePattern);
     }
 
 
-//        assertNotNull(mojo.outputDirectory);
-//
-//        /* check default values */
-//        // check outputDirectory
-//        final Path plantumlTargetDir = Paths.get("target/plantuml");
-//        assertEquals(plantumlTargetDir.toAbsolutePath().toFile(), mojo.outputDirectory);
-//        // outputInSourceDirectory
-//        assertFalse(mojo.outputInSourceDirectory);
-//        // verbose
-//        assertFalse(mojo.verbose);
-//
-//        /* check required parameters */
-//        assertNotNull(mojo.sourceFiles);
-//        assertEquals(Paths.get("").toAbsolutePath().toString(), mojo.sourceFiles.getDirectory().toString());
-//
-//        /* check others parameters */
-//        assertNotNull(mojo.truncatePattern);
-//        assertEquals("src/main/*", mojo.truncatePattern);
-//    }
 }
